878de9563a6eb0a76ff1b0e7aba914891eeb8767
Marco Girardi <marco.girardi@factorymind.com>
2025-12-01 13:32:03 +0100

    partone

--- day01.py
+++ day01.py
@@ -1,4 +1,47 @@
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from utils.input import parse_lines
+
+class Direction(Enum):
+    L = 1
+    R = 2
+
+def to_direction(direction_string: str) -> Direction:
+    if direction_string == "L":
+        return Direction.L
+    if direction_string == "R":
+        return Direction.R
+    raise ValueError(f"Invalid direction: {direction_string}")
 
 
-def solve(input: list[str]) -> int:
-    return 42
+@dataclass
+class Move:
+    direction: Direction
+    steps: int
+
+def solve(source: list[str]) -> int:
+    count = 0
+    current = 50
+    for line in source:
+        direction_string = line[0]
+        movement_amount = int(line[1:])
+        move = Move(to_direction(direction_string), movement_amount)
+
+        # this is the closest thing to a switch case apparently
+        match move.direction:
+            case Direction.L:
+                current = (current + move.steps) % 100
+            case Direction.R:
+                current = (current + 100 - move.steps) % 100
+
+        if current == 0:
+            count = count + 1
+
+    return count
+
+
+
+input = Path(__file__).parent / "01.txt"
+result = solve(parse_lines(input))
+print(result)
