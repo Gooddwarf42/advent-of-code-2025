5cb1515bf2fbe5c88558b9ca1737d19249ef9ada
Marco Girardi <marco.girardi@factorymind.com>
2025-12-02 12:36:05 +0100

    main magic. Ugly part one

--- day02.py
+++ day02.py
@@ -1,29 +1,63 @@
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
-from utils.input import parse_lines
+from utils.input import parse_lines, parse
 
 DAY = "02"
 
 
-def solve_part1(source: list[str]) -> int:
-    count = 0
+@dataclass()
+class Range:
+    lower_bound: int
+    upper_bound: int
 
+
+def get_ranges(source: str) -> list[Range]:
+    raw_ranges = source.split(',')
+    ranges: list[Range] = []
+    for raw_range in raw_ranges:
+        bounds = raw_range.split('-')
+        range_to_append = Range(int(bounds[0]), int(bounds[1]))
+        ranges.append(range_to_append)
+
+    return ranges
+
+
+def solve_part1(source: str) -> int:
+    count = 0
+    ranges = get_ranges(source)
+    # let's make it stupid for now. I hate this. But I want to see part 2
+    for interval in ranges:
+        value = interval.lower_bound
+        while value <= interval.upper_bound:
+            value_string = str(value)
+
+            length = len(value_string)
+            if length % 2 != 0:
+                value = value + 1
+                continue
+
+            substring_length = length // 2
+            left = value_string[:substring_length]
+            right = value_string[substring_length:]
+
+            if left == right:
+                count = count + value
+
+            value = value + 1
 
     return count
 
 
-def solve_part2(source: list[str]) -> int:
+def solve_part2(source: str) -> int:
     count = 0
-
 
     return count
 
-
-
-file = Path(__file__).parent / f"{DAY}.txt"
-parsed = parse_lines(file)
-result = solve_part1(parsed)
-print(result)
-result = solve_part2(parsed)
-print(result)
+if __name__ == "__main__":
+    file = Path(__file__).parent / f"{DAY}.txt"
+    parsed = parse(file)
+    result = solve_part1(parsed)
+    print(result)
+    result = solve_part2(parsed)
+    print(result)
--- tests/test_day02.py
+++ tests/test_day02.py
@@ -1,18 +1,27 @@
 from pathlib import Path
 
-from day02 import solve_part1, solve_part2
-from utils.input import parse_lines
+from day02 import solve_part1, solve_part2, get_ranges, Range
+from utils.input import parse_lines, parse
 
 DAY = "02"
 
+
 def test_day02_solve_part1() -> None:
     path = Path(__file__).parent / f"{DAY}.txt"
-    input = parse_lines(path)
+    input = parse(path)
     solution = solve_part1(input)
     assert solution == 1227775554
 
+
 def test_day02_solve_part2() -> None:
     path = Path(__file__).parent / f"{DAY}.txt"
-    input = parse_lines(path)
+    input = parse(path)
     solution = solve_part2(input)
     assert solution == 420
+
+
+def test_get_ranges():
+    source = "0-7,09-15"
+    result = get_ranges(source)
+    expected_ranges = [Range(0, 7), Range(9, 15)]
+    assert result == expected_ranges
