9a39d285fb70f0f86e3d575f690b552116416f06
Gooddwarf42 <33847534+Gooddwarf42@users.noreply.github.com>
2025-12-04 12:43:22 +0100

    04 (#4)
    * sethup
    
    * PartOne
    
    * Part two
    
    ---------
    
    Co-authored-by: Marco Girardi <marco.girardi@factorymind.com>

---
+++ day04.py
@@ -0,0 +1,96 @@
+import time
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from utils.input import parse_lines, parse
+
+DAY = "04"
+
+def is_oob (value:int , lb:int, ub:int) -> bool:
+    """
+    checks whether value is out of bounds for range between lb and ub, lb included, ub excluded
+    :param value:
+    :param lb:
+    :param ub:
+    :return:
+    """
+    return value < lb or value >= ub
+
+def count_neighbours(source: list[str], i:int, j:int) -> int:
+    count = 0
+    cells_to_check = [
+        [i-1, j-1],
+        [i-1, j],
+        [i-1, j+1],
+        [i, j-1],
+        [i, j+1],
+        [i+1, j-1],
+        [i+1, j],
+        [i+1, j+1],
+    ]
+
+    upper_bound_i = len(source)
+    upper_bound_j = len(source[0])
+
+    for (target_i, target_j) in cells_to_check:
+        if is_oob(target_i, 0, upper_bound_i) or is_oob(target_j, 0, upper_bound_j):
+            continue
+        cell_value = source[target_i][target_j]
+        if cell_value == "@":
+            count = count + 1
+    return count
+
+
+def solve_part1(source: list[str]) -> int:
+    count = 0
+
+    for i in range(len(source)):
+        line = source[i]
+        for j in range(len(line)):
+            if source[i][j] != "@":
+                continue
+            neighbours = count_neighbours(source, i, j)
+            if neighbours < 4:
+                count = count + 1
+
+    return count
+
+def solve_part2(source: list[str]) -> int:
+    count = 0
+    iteration_count = -1
+    while iteration_count != 0:
+        iteration_count = 0
+
+        new_source = source.copy()
+
+        for i in range(len(source)):
+            line = source[i]
+            for j in range(len(line)):
+                if source[i][j] != "@":
+                    continue
+                neighbours = count_neighbours(source, i, j)
+                if neighbours < 4:
+                    iteration_count = iteration_count + 1
+                    new_source[i] = new_source[i][:j] + "." + new_source[i][j+1:] #not really elegant
+
+        count = count+ iteration_count
+        source = new_source
+
+
+    return count
+
+
+if __name__ == "__main__":
+    file = Path(__file__).parent / f"{DAY}.txt"
+    parsed = parse_lines(file)
+    start = time.perf_counter()
+    result = solve_part1(parsed)
+    end = time.perf_counter()
+    print(f"Solved part 1 in {end - start: .6f} seconds")
+    print(result)
+
+    start = time.perf_counter()
+    result = solve_part2(parsed)
+    end = time.perf_counter()
+    print(f"Solved part 2 in {end - start: .6f} seconds")
+    print(result)
---
+++ tests/test_day04.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pytest
+
+from day04 import solve_part1, solve_part2, count_neighbours
+from utils.input import parse_lines, parse
+
+DAY = "04"
+
+
+def test_day04_solve_part1() -> None:
+    path = Path(__file__).parent / f"{DAY}.txt"
+    input = parse_lines(path)
+    solution = solve_part1(input)
+    assert solution == 13
+
+
+def test_day04_solve_part2() -> None:
+    path = Path(__file__).parent / f"{DAY}.txt"
+    input = parse_lines(path)
+    solution = solve_part2(input)
+    assert solution == 43
+
+@pytest.mark.parametrize(
+    ("i", "j", "expected"),
+    [
+        (0, 2, 2),
+        (1, 0, 2),
+        (1, 3, 5),
+    ]
+)
+def test_count_neighbours(i: int, j:int, expected: int):
+    test_case = [
+        "@..@@@",
+        "@..@@@",
+        "@..@@@",
+        ]
+    assert count_neighbours(test_case, i, j) == expected
+
