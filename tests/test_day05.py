6063f1b394c0a983dd7642aacc8b4911ce7c4a63
Marco Girardi <marco.girardi@factorymind.com>
2025-12-05 15:22:41 +0100

    asdasd

--- tests/test_day05.py
+++ tests/test_day05.py
@@ -20,7 +20,7 @@ def test_day04_solve_part2() -> None:
     path = Path(__file__).parent / f"{DAY}.txt"
     input = parse_lines(path)
     solution = solve_part2(input)
-    assert solution == 420
+    assert solution == 14
 
 
 def test_parse_day_5():
