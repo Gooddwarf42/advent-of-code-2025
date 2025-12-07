092e728f900b66a0727d760c4ee121c66942460a
marco <marcogirarditesero@gmail.com>
2025-12-07 21:43:48 +0100

    PartOne

--- day07.py
+++ day07.py
@@ -5,12 +5,32 @@ from pathlib import Path
 from typing import Tuple
 
 from utils.input import parse_lines, parse
+from utils.list import distinct
 from utils.range import Range, get_ranges_from_lines, is_in_bound
 
 DAY = "07"
 
 def solve_part1(source: list[str]) -> int:
-    return 0
+    count = 0
+    beams: list[int] = [source[0].index("S")]
+    
+    for line in source[1:]:
+        next_beams = []
+        for beam_index in beams:
+            if line[beam_index] != "^":
+                next_beams.append(beam_index)
+                continue
+            
+            if beam_index > 0:
+                next_beams.append(beam_index - 1)
+
+            if beam_index < len(line) - 1:
+                next_beams.append(beam_index + 1)
+
+            count = count + 1
+            beams = distinct(next_beams)
+            
+    return count
 
 
 def solve_part2(source: list[str]) -> int:
--- tests/test_list.py
+++ tests/test_list.py
@@ -1,17 +1,28 @@
 import pytest
 
-from utils.list import split_list
+from utils.list import split_list, distinct
 
 
 @pytest.mark.parametrize(
     ("source", "separator", "expected"),
     [
-        (["a","baba","c","","rarw"], "", [["a","baba","c"],["rarw"]]),
-        ([1,2,3,4,5], 3, [[1,2], [4,5]]),
-        ([1,2,3,4,5], 6, [[1,2,3,4,5]]),
-        ([1,2,3,4,5], 5, [[1,2,3,4]]),
-        ([1,2,3,4,5], 1, [[2,3,4,5]]),
+        (["a", "baba", "c", "", "rarw"], "", [["a", "baba", "c"], ["rarw"]]),
+        ([1, 2, 3, 4, 5], 3, [[1, 2], [4, 5]]),
+        ([1, 2, 3, 4, 5], 6, [[1, 2, 3, 4, 5]]),
+        ([1, 2, 3, 4, 5], 5, [[1, 2, 3, 4]]),
+        ([1, 2, 3, 4, 5], 1, [[2, 3, 4, 5]]),
     ]
 )
 def test_split_list(source, separator, expected):
     assert split_list(source, separator) == expected
+
+@pytest.mark.parametrize(
+    ("source", "expected"),
+    [
+        (["a", "ba", "c", "a", ""], ["a", "ba", "c", ""]),
+        ([1,2,3,1,2,3], [1,2,3]),
+        ([1,2,3,2,2,3], [1,2,3]),
+    ]
+)
+def test_distinct(source, expected):
+    assert distinct(source) == expected
