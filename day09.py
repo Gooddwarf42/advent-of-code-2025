9375b76d988d355b1394373d8e6002c7fcd053c1
marco <marcogirarditesero@gmail.com>
2025-12-13 00:12:33 +0100

    Cleanup

--- day09.py
+++ day09.py
@@ -27,7 +27,8 @@ class WallInfo:
     index: int
     ran: Range
     vertical: bool
-    
+
+
 ######### DRAWING STUFF #############
 
 def ready_canvas(points: list[Point2d], scale: float) -> tuple[Tk, tk.Canvas, list[tuple[float, float]]]:
@@ -54,10 +55,13 @@ def draw_lines(canvas, pts: list[tuple[float, float]], color="black", width=2):
     for pt in pts:
         canvas.create_oval(pt[0] - r, pt[1] - r, pt[0] + r, pt[1] + r, fill=color)
 
-def draw_rectangle(canvas: tk.Canvas, pts: list[tuple[float, float]], first: int, second: int, color="magenta", width=2):
+
+def draw_rectangle(canvas: tk.Canvas, pts: list[tuple[float, float]], first: int, second: int, color="magenta",
+                   width=2):
     marmellata = pts[first]
     giusteppe = pts[second]
     canvas.create_rectangle(marmellata[0], marmellata[1], giusteppe[0], giusteppe[1], fill=color, width=width)
+
 
 #####################################
 
@@ -76,7 +80,7 @@ def get_area_info(points: list[Point2d]) -> list[AreaInfo]:
     return res
 
 
-def find_vertical_walls(points: list[Point2d]) -> list[WallInfo]:
+def find_walls(points: list[Point2d]) -> list[WallInfo]:
     walls: list[WallInfo] = []
 
     for i in range(len(points)):
@@ -84,65 +88,18 @@ def find_vertical_walls(points: list[Point2d]) -> list[WallInfo]:
         next_point = points[(i + 1) % len(points)]
         if this_point.y == next_point.y:
             # horizontal wall
+            walls.append(
+                WallInfo(this_point.y, Range(min(this_point.x, next_point.x), max(this_point.x, next_point.x)), False))
             continue
-        walls.append(WallInfo(this_point.x, Range(min(this_point.y, next_point.y), max(this_point.y, next_point.y)), True))
+        walls.append(
+            WallInfo(this_point.x, Range(min(this_point.y, next_point.y), max(this_point.y, next_point.y)), True))
 
-    walls.sort(key=lambda info: info.index)
-    return walls
-
-def find_horizontal_walls(points: list[Point2d]) -> list[WallInfo]:
-    walls: list[WallInfo] = []
-
-    for i in range(len(points)):
-        this_point = points[i]
-        next_point = points[(i + 1) % len(points)]
-        if this_point.x == next_point.x:
-            # horizontal wall
-            continue
-        walls.append(WallInfo(this_point.y, Range(min(this_point.x, next_point.x), max(this_point.x, next_point.x)), False))
-
-    walls.sort(key=lambda info: info.index)
+    # Sorting all vertical walls first basically halves execution time for part 2
+    walls.sort(key=lambda w: w.vertical)
     return walls
 
 
-def is_wall_on_left(wall: WallInfo, point: Point2d) -> bool:
-    if wall.index >= point.x:
-        return False
-    return is_in_bound(point.y, wall.ran, False, True)
-
-
-def is_wall_on_right(wall: WallInfo, point: Point2d) -> bool:
-    if wall.index <= point.x:
-        return False
-    return is_in_bound(point.y, wall.ran, False, True)
-
-def has_wall_on_left(wall: WallInfo, walls: list[WallInfo]) -> bool:
-    def is_wall_on_left_of_a_wall(source:WallInfo, target: WallInfo) -> bool:
-        if target.index >= source.index:
-            return False
-        if target.ran.lower_bound >= source.ran.upper_bound:
-            return False
-        if target.ran.upper_bound <= source.ran.lower_bound:
-            return  False
-        return True
-
-    walls_on_left= count(walls, lambda w: is_wall_on_left_of_a_wall(wall, w))
-    return walls_on_left > 0
-
-def has_wall_on_right(wall: WallInfo, walls: list[WallInfo]) -> bool:
-    def is_wall_on_right_of_a_wall(source:WallInfo, target: WallInfo) -> bool:
-        if target.index <= source.index:
-            return False
-        if target.ran.lower_bound >= source.ran.upper_bound:
-            return False
-        if target.ran.upper_bound <= source.ran.lower_bound:
-            return  False
-        return True
-
-    walls_on_right= count(walls, lambda w: is_wall_on_right_of_a_wall(wall, w))
-    return walls_on_right > 0
-
-def rectangle_collides_with_wall(first_point: Point2d, second_point: Point2d, wall : WallInfo) -> bool:
+def rectangle_collides_with_wall(first_point: Point2d, second_point: Point2d, wall: WallInfo) -> bool:
     caccola = [first_point, second_point]
     max_x = max([point.x for point in caccola])
     max_y = max([point.y for point in caccola])
@@ -153,7 +110,7 @@ def rectangle_collides_with_wall(first_point: Point2d, second_point: Point2d, wa
         if not min_x < wall.index < max_x:
             return False
         if wall.ran.lower_bound >= max_y:
-            return  False
+            return False
         if wall.ran.upper_bound <= min_y:
             return False
         return True
@@ -165,7 +122,6 @@ def rectangle_collides_with_wall(first_point: Point2d, second_point: Point2d, wa
     if wall.ran.upper_bound <= min_x:
         return False
     return True
-    
 
 
 def solve_part1(source: list[str]) -> int:
@@ -178,145 +134,36 @@ def solve_part1(source: list[str]) -> int:
 def solve_part2(source: list[str]) -> int:
     points = get_points2d(source)
 
-    max_x = max([point.x for point in points])
-    max_y = max([point.y for point in points])
+    walls = find_walls(points)
 
-    min_x = min([point.x for point in points])
-    min_y = min([point.y for point in points])
-
-    on_top = count(points, lambda p: p.y == min_y)
-    on_left = count(points, lambda p: p.x == min_x)
-    on_bottom = count(points, lambda p: p.y == max_y)
-    on_right = count(points, lambda p: p.x == max_x)
-
-    print([on_top, on_left, on_right, on_bottom])
-    # this gives us [2,2,2,2]. We can rid of some annoying edge cases then!
-
-    # just for the sake of more edge case avoidance, I wanna see if we alternate left and right turns
-    turns: list[int] = []
-    for i in range(len(points)):
-        this_point = points[i]
-        next_point = points[(i + 1) % len(points)]
-        prev_point = points[(len(points) + i - 1) % len(points)]
-        vec_prev = [prev_point.x - this_point.x, prev_point.y - this_point.y]
-        vec_next = [next_point.x - this_point.x, next_point.y - this_point.y]
-        turn = vec_prev[0] * vec_next[1] - vec_prev[1] * vec_next[0]
-        turns.append(abs(turn) // turn)
-
-    evil = []
-    for i in range(len(turns)):
-        if turns[i] != turns[(i + 1) % len(turns)]:
-            continue
-        turn_type = "L" if turns[i] == 1 else "R"
-        print(f"Same turn ({turn_type}) detected at index {i}: point {points[i]}")
-        evil.append([i, turn_type])
-
-    print(len(evil))
-    print(evil)
-
-    # what about three times same turn?
-    evil = []
-    for i in range(len(turns)):
-        if not (turns[i] == turns[(i + 1) % len(turns)] == turns[(i + 2) % len(turns)]):
-            continue
-        turn_type = "L" if turns[i] == 1 else "R"
-        print(f"DOUBLE Same turn ({turn_type}) detected at index {i}: point {points[i]}")
-        evil.append([i, turn_type])
-
-    print(len(evil))
-    print(evil)
-    # Todo: find bounding rectangles (a set of bounding rectangles per corner, easily identified by their opposite vertexes)
-    # iterate on all rectangles (in decreasing order of area) and check collision with any of the bounding rectangles using AABB
-    # TODO uffa non penso andrà. Piango
-
-    vertical_walls = find_vertical_walls(points)
-    good_left_walls: list[WallInfo] = []
-    good_right_walls: list[WallInfo] = []
-    for wall in vertical_walls:
-        if not has_wall_on_left(wall, vertical_walls):
-            good_left_walls.append(wall)
-            continue
-        if not has_wall_on_right(wall, vertical_walls):
-            good_right_walls.append(wall)
-            continue
-
-
-    horizontal_walls = find_horizontal_walls(points)
-    good_top_walls: list[WallInfo] = []
-    good_bottom_walls: list[WallInfo] = []
-    for wall in horizontal_walls:
-        if not has_wall_on_left(wall, horizontal_walls):
-            good_top_walls.append(wall)
-            continue
-        if not has_wall_on_right(wall, horizontal_walls):
-            good_bottom_walls.append(wall)
-            continue
-
-    # drawiamo qualche cagatina
-    scale = 0.0115
-    #scale = 30.0
-    root, canvas, scaled_points = ready_canvas(points, scale)
-
-    scaled_vert_walls = [[(wall.index * scale, wall.ran.lower_bound * scale), (wall.index * scale, wall.ran.upper_bound * scale)] for wall in vertical_walls]
-    for scaled_wall in scaled_vert_walls:
-        draw_lines(canvas, scaled_wall, color="red", width=1)
-
-    scaled_good_left_vert_walls = [[(wall.index * scale, wall.ran.lower_bound * scale), (wall.index * scale, wall.ran.upper_bound * scale)] for wall in good_left_walls]
-    for scaled_wall in scaled_good_left_vert_walls:
-        draw_lines(canvas, scaled_wall, color="green", width=6)
-
-    scaled_good_right_vert_walls = [[(wall.index * scale, wall.ran.lower_bound * scale), (wall.index * scale, wall.ran.upper_bound * scale)] for wall in good_right_walls]
-    for scaled_wall in scaled_good_right_vert_walls:
-        draw_lines(canvas, scaled_wall, color="orange", width=6)
-        
-    scaled_hor_walls = [[(wall.ran.lower_bound * scale, wall.index * scale), (wall.ran.upper_bound * scale, wall.index * scale)] for wall in horizontal_walls]
-    for scaled_wall in scaled_hor_walls:
-        draw_lines(canvas, scaled_wall, color="red", width=1)
-
-    scaled_good_left_hor_walls = [[(wall.ran.lower_bound * scale, wall.index * scale,), (wall.ran.upper_bound * scale, wall.index * scale)] for wall in good_top_walls]
-    for scaled_wall in scaled_good_left_hor_walls:
-        draw_lines(canvas, scaled_wall, color="green", width=6)
-
-    scaled_good_right_hor_walls = [[(wall.ran.lower_bound * scale, wall.index * scale), (wall.ran.upper_bound * scale, wall.index * scale)] for wall in good_bottom_walls]
-    for scaled_wall in scaled_good_right_hor_walls:
-        draw_lines(canvas, scaled_wall, color="orange", width=6)
-
-    root.mainloop()
-
-    # proviamo
     area_info = get_area_info(points)
     area_info.reverse()
 
-    el_cachone: dict[Point2d, bool] = {}
-
-    tot = len(area_info)
-    i = 0
     for info in area_info:
-        i = i+1
-        print(f"checking rectancle {i}/{tot}")
         is_good = True
-        for wall in horizontal_walls + vertical_walls:
+        for wall in walls:
             if rectangle_collides_with_wall(points[info.first_index], points[info.second_index], wall):
                 is_good = False
-                #print(f"Collision detected: rectangle {info.first_index} - {info.second_index} collides with {wall}")
-                #root, canvas, scaled_points = ready_canvas(points, scale)
-                #draw_rectangle(canvas, scaled_points, info.first_index, info.second_index)
-                #scaled_wall = [( wall.index * scale, wall.ran.lower_bound * scale), ( wall.index * scale, wall.ran.upper_bound * scale)] if wall.vertical else [(wall.ran.lower_bound * scale, wall.index * scale), (wall.ran.upper_bound * scale, wall.index * scale)]
-                #draw_lines(canvas, scaled_wall, color="orange", width=6)
-                #root.mainloop()
-                
+                # print(f"Collision detected: rectangle {info.first_index} - {info.second_index} collides with {wall}")
+                # root, canvas, scaled_points = ready_canvas(points, scale)
+                # draw_rectangle(canvas, scaled_points, info.first_index, info.second_index)
+                # scaled_wall = [( wall.index * scale, wall.ran.lower_bound * scale), ( wall.index * scale, wall.ran.upper_bound * scale)] if wall.vertical else [(wall.ran.lower_bound * scale, wall.index * scale), (wall.ran.upper_bound * scale, wall.index * scale)]
+                # draw_lines(canvas, scaled_wall, color="orange", width=6)
+                # root.mainloop()
+
                 break
-        
+
         if not is_good:
             continue
-            
+
         print(f"THIS ONE IS GOOD! {info}")
 
+        scale = 0.0115
         root, canvas, scaled_points = ready_canvas(points, scale)
         draw_rectangle(canvas, scaled_points, info.first_index, info.second_index)
         root.mainloop()
         return info.area
-        
+
     return 0
 
 
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from day09 import solve_part1, solve_part2, AreaInfo, get_area_info, find_vertical_walls, WallInfo, is_wall_on_left, is_wall_on_right, has_wall_on_left, has_wall_on_right
+from day09 import solve_part1, solve_part2, AreaInfo, get_area_info, WallInfo, find_walls
 from utils.input import parse_lines, parse
 from utils.points import Point3d, Point2d, get_points2d
 from utils.range import Range
@@ -37,75 +37,3 @@ def test_get_area_info():
         AreaInfo(2, 0, 2),
     ]
     assert get_area_info(source) == expected
-
-
-def test_find_vertical_walls():
-    path = Path(__file__).parent / f"{DAY}.txt"
-    input = parse_lines(path)
-    points = get_points2d(input)
-    walls = find_vertical_walls(points)
-    assert walls == [
-        WallInfo(2, Range(3, 5)),
-        WallInfo(7, Range(1, 3)),
-        WallInfo(9, Range(5, 7)),
-        WallInfo(11, Range(1, 7)),
-    ]
-
-
-@pytest.mark.parametrize(
-    ("wall", "point", "expected"),
-    [
-        (WallInfo(1, Range(2, 3), True), Point2d(2, 2), True),
-        (WallInfo(3, Range(2, 3), True), Point2d(2, 2), False),
-        (WallInfo(2, Range(2, 3), True), Point2d(2, 2), False),
-        (WallInfo(1, Range(2, 3), True), Point2d(2, 1), False),
-        (WallInfo(1, Range(2, 3), True), Point2d(2, 3), True),
-    ]
-)
-def test_is_wall_on_left(wall: WallInfo, point: Point2d, expected: bool):
-    assert is_wall_on_left(wall, point) == expected
-
-
-@pytest.mark.parametrize(
-    ("wall", "point", "expected"),
-    [
-        (WallInfo(1, Range(2, 3), True), Point2d(2, 2), False),
-        (WallInfo(3, Range(2, 3), True), Point2d(2, 2), True),
-        (WallInfo(2, Range(2, 3), True), Point2d(2, 2), False),
-        (WallInfo(3, Range(2, 3), True), Point2d(2, 1), False),
-        (WallInfo(3, Range(2, 3), True), Point2d(2, 3), True),
-        (WallInfo(3, Range(2, 3), True), Point2d(2, 3), True),
-    ]
-)
-def test_is_wall_on_right(wall: WallInfo, point: Point2d, expected: bool):
-    assert is_wall_on_right(wall, point) == expected
-
-@pytest.mark.parametrize(
-    ("wall", "walls", "expected"),
-    [
-        (WallInfo(1, Range(2, 3), True), [WallInfo(0, Range(2, 3), True)], True),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(0, Range(0, 1), True)], False),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(0, Range(0, 2), True)], True),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(0, Range(3, 4), True)], True),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(0, Range(4, 5), True)], False),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(1, Range(2, 3), True)], False),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(2, Range(2, 3), True)], False),
-    ]
-)
-def test_has_wall_on_left(wall: WallInfo, walls: list[WallInfo], expected: bool):
-    assert has_wall_on_left(wall, walls) == expected
-
-@pytest.mark.parametrize(
-    ("wall", "walls", "expected"),
-    [
-        (WallInfo(1, Range(2, 3), True), [WallInfo(0, Range(2, 3), True)], False),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(1, Range(2, 3), True)], False),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(2, Range(2, 3), True)], True),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(2, Range(0, 1), True)], False),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(2, Range(0, 2), True)], True),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(2, Range(3, 4), True)], True),
-        (WallInfo(1, Range(2, 3), True), [WallInfo(2, Range(4, 5), True)], False),
-    ]
-)
-def test_has_wall_on_right(wall: WallInfo, walls: list[WallInfo], expected: bool):
-    assert has_wall_on_right(wall, walls) == expected
